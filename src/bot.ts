01060c6f70091581243e982bd101484999250a63
primepvi <pedrobernardesv@gmail.com>
2026-06-06 18:42:37 -0300

    feat(bot): implementing bot init in project entrypoint.

---
+++ src/bot.ts
@@ -0,0 +1,14 @@
+import { GatewayIntentBits } from "discord.js";
+import { Bot } from "./structs/bot.js";
+
+export const bot = new Bot({
+	token: process.env.BOT_TOKEN!,
+	intents: [
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildPresences
+	]
+});
